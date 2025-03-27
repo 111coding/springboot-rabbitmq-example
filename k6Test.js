43c0e369ef12986c0d39f3ad1c9ae5c758d12fee
111coding <jwlee1024@gmail.com>
2025-03-27 22:58:11 +0800

    test: add check response

--- k6Test.js
+++ k6Test.js
@@ -1,16 +1,20 @@
 import http from "k6/http";
+import { check } from "k6";
 
 export const options = {
-  vus: 1000,
+  vus: 100,
   iterations: 1000,
 };
 
 // k6 테스트 진입점(시작점)
 // 설정한 반복수(iterations) 만큼 실행
 export default function () {
-  http.post(
+  const response = http.post(
     "http://localhost:8080/order",
     JSON.stringify({ productId: 1, userId: __VU }),
     { headers: { "Content-Type": "application/json" } }
   );
+  check(response, {
+    "is status CREATED": (r) => r.status === 201,
+  });
 }
